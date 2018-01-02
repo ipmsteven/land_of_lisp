8f8d55332d8e5feeb4b45e9fc409770ecefa8a52
ipmsteven <steven.lyl147@gmail.com>
2018-01-01 22:49:32 -0800

    add game-eval

--- ch-6/custom-repl.lisp
+++ ch-6/custom-repl.lisp
@@ -19,3 +19,9 @@
              (list 'quote x)))
       (cons (car cmd) (mapcar #'quote-it (cdr cmd))))))
 (game-read)
+
+(defparameter *allowed-commands* '(look walk pickup inventory))
+(defun game-eval (sexp)
+  (if (member (car sexp) *allowed-commands*)
+      (eval sexp)
+      '(i do not know that command.)))
