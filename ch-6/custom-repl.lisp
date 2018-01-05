0fc32e8b2ca345d043685832e7faf019f63aab46
ipmsteven <steven.lyl147@gmail.com>
2018-01-04 23:12:48 -0800

    add game-print and its friends

--- ch-6/custom-repl.lisp
+++ ch-6/custom-repl.lisp
@@ -10,7 +10,7 @@
       (game-print (game-eval cmd))
       (game-repl-2))))
 
-(game-repl-2)
+;; (game-repl-2)
 
 (defun game-read ()
   (let ((cmd (read-from-string
@@ -18,10 +18,32 @@
     (flet ((quote-it (x)
              (list 'quote x)))
       (cons (car cmd) (mapcar #'quote-it (cdr cmd))))))
-(game-read)
+;; (game-read)
 
 (defparameter *allowed-commands* '(look walk pickup inventory))
 (defun game-eval (sexp)
   (if (member (car sexp) *allowed-commands*)
       (eval sexp)
       '(i do not know that command.)))
+
+(defun tweak-text (lst caps lit)
+  (when lst
+    (let ((item (car lst))
+          (rest (cdr lst)))
+      (cond ((eql item #\space) (cons item (tweak-text rest caps lit)))
+            ((member item '(#\! #\? #\.)) (cons item (tweak-text rest t lit)))
+            ((eql item #\") (tweak-text rest caps (not lit)))
+            (lit (cons item (tweak-text rest nil lit)))
+            (caps (cons (char-upcase item) (tweak-text rest nil lit)))
+            (t (cons (char-downcase item) (tweak-text rest nil nil)))))))
+(defun game-print (lst)
+  (princ (coerce (tweak-text (coerce (string-trim "() "
+                                                  (prin1-to-string lst))
+                                     'list)
+                             t
+                             nil)
+                 'string))
+  (fresh-line))
+
+(game-print '(this is a ttest. haha? hehe! hehe))
+
